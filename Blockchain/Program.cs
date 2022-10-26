5fd55fcf78569277283c6b4a2154106f81cfdba6
AlinaRinn <nik17042001@rambler.ru>
2022-10-26 17:43:57 +0300

    No excel solution

--- Blockchain/Program.cs
+++ Blockchain/Program.cs
@@ -1,6 +1,4 @@
-﻿using ClosedXML.Excel;
-using System.Diagnostics;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 namespace Blockchain;
@@ -116,32 +114,7 @@ public class Program
 
         Console.WriteLine("\n============================\n");
     }// Метод для вывода данных в консоль.
-    static void PrintExcel(List<Chain> chains, string name)
-    {
 
-        string? path = Path.Combine(Environment.CurrentDirectory, name);
-        XLWorkbook? wb = new();
-        IXLWorksheet? sh = wb.Worksheets.Add("1");
-        sh.Cell(1, 1).SetValue("Номер цепи");
-        sh.Cell(1, 2).SetValue("Операция");
-        sh.Cell(1, 3).SetValue("Текущий хеш");
-        sh.Cell(1, 4).SetValue("Предыдущий хеш");
-
-        for (int i = 0; i < chains.Count; i++)
-        {
-            List<Node> nodes = chains[i].List;
-            for (int j = 0; j < nodes.Count; j++)
-            {
-                sh.Cell(i * nodes.Count + j + 2, 1).SetValue(i);
-                sh.Cell(i * nodes.Count + j + 2, 2).SetValue(nodes[j].Operation);
-                sh.Cell(i * nodes.Count + j + 2, 3).SetValue(nodes[j].CurrentHash);
-                sh.Cell(i * nodes.Count + j + 2, 4).SetValue(nodes[j].PreviousHash);
-            }
-
-        }
-
-        wb.SaveAs(path + ".xlsx");
-    }// Метод для вывода данных в Excel.
     static void PrintCSV(List<Chain> chain, string message = "")
     {
         StreamWriter sw = new("Output.csv");
@@ -151,7 +124,7 @@ public class Program
         {
             sw.WriteLine("Chain " + i + ":\t");
             foreach (var item in chain[i].List)
-                sw.WriteLine(item.Operation + ";" + item.PreviousHash + ";" + item.CurrentHash + ",");
+                sw.WriteLine(item.Operation + ";" + item.PreviousHash + ";" + item.CurrentHash);
             sw.WriteLine();
         }
 
